2a20c6331a74e113b96978de4b10488feea8e7d1
Aziz Tazrout <aztazrout@gmail.com>
2025-03-14 11:40:04 -0400

    Setting up the Athlex project

---
+++ src/main/java/com/azed/athlex/AthlexApplication.java
@@ -0,0 +1,13 @@
+package com.azed.athlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthlexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azed/athlex/AthlexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azed.athlex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthlexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
