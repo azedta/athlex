f2a3095df040c4cb8dfd5f437417c4e1a5b434bd
Aziz Tazrout <aztazrout@gmail.com>
2025-03-19 16:03:01 -0400

    Add Validations for Category module

--- src/main/java/com/azed/athlex/repositories/CategoryRepository.java
+++ src/main/java/com/azed/athlex/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.azed.athlex.repositories;
 
 import com.azed.athlex.model.Category;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(@NotBlank @Size(min = 5, message = "Category name must contain at least 5 characters") String categoryName);
 }
--- src/main/java/com/azed/athlex/service/CategoryServiceImpl.java
+++ src/main/java/com/azed/athlex/service/CategoryServiceImpl.java
@@ -1,38 +1,42 @@
 package com.azed.athlex.service;
 
+import com.azed.athlex.exceptions.APIException;
+import com.azed.athlex.exceptions.ResourceNotFoundException;
 import com.azed.athlex.model.Category;
 import com.azed.athlex.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-
-    //private List<Category> categories = new ArrayList<>();
 
     @Autowired
     private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty()) {
+            throw new APIException("Category not found");
+        }
+        return categories;
     }
 
     @Override
     public void createCategory(Category category) {
+        Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
+        if (savedCategory != null) {
+            throw new APIException("Category with the name: " + category.getCategoryName() + " already exists !!");
+        }
         categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
         Category category = categoryRepository.findById(categoryId)
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found") );
+                .orElseThrow( () -> new ResourceNotFoundException("Category","categoryId",categoryId) );
 
         categoryRepository.delete(category);
         return "Category with id : " + categoryId + " deleted successfully.";
@@ -41,7 +45,7 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Category updateCategory(Category category, Long categoryId) {
         Category savedCategory = categoryRepository.findById(categoryId)
-                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found") );
+                .orElseThrow( () -> new ResourceNotFoundException("Category","categoryId",categoryId) );
 
         category.setCategoryId(categoryId);
         savedCategory = categoryRepository.save(category);
